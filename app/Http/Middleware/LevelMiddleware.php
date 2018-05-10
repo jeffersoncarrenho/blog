7a639359769bd832bda2aec104f7c8a207354a2c
jeffersoncarrenho <jefferson.carrenho@gmail.com>
2018-05-10 18:20:07 -0300

    Aula 3

---
+++ app/Http/Middleware/LevelMiddleware.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class LevelMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next, $level)
+    {
+      if (Auth()::user()->level>=$level) {
+        return $next($request);
+      }else {
+        return redirect('/');
+      }
+    }
+}
